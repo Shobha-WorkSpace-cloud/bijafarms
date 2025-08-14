4d9be2188f39c31a418387a80c54ce1abb241c3d
Shobha-WorkSpace-cloud <shobha.sangi@outlook.com>
2025-08-15 02:42:00 +0530

    Update api-config.ts

--- client/lib/api-config.ts
+++ client/lib/api-config.ts
@@ -29,7 +29,7 @@ const getApiBaseUrl = (): string => {
 
     // Enable mock mode for development, localhost, or builder.io development environments
     if (isLocalhost || isDevelopment || isBuilderDev) {
-      return "https://bijafarms-api.onrender.com/api";// "__MOCK_MODE__";
+      return  "__MOCK_MODE__";
     }
   }
 
